fa3698204199b3cc10c1c85e438048baf08a1dd7
Emma Gouti <goutiemma@gmail.com>
2018-09-30 18:25:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  
+def fizzbuzz(x)
+  if x % 3 == 0 
+    puts "Fizz"
+    
+    elsif x % 5 == 0 
+    puts "Buzz"
+    
+  else 
+    puts "fizzbuzz"
+  end
 end
