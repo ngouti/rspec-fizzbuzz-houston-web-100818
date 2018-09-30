5f41ae44001edb78e9816c64399ea2fc055a3349
Emma Gouti <goutiemma@gmail.com>
2018-09-30 18:26:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
   if x % 3 == 0 
-    puts "Fizz"
+     "Fizz"
     
     elsif x % 5 == 0 
-    puts "Buzz"
+     "Buzz"
     
   else 
-    puts "FizzBuzz"
+     "FizzBuzz"
   end
 end
